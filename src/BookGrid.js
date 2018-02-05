168f1222efeece5d7bbdc32d7b05a34f9aa05b81
Adhik N A <adhikn.a2015@vit.ac.in>
2018-02-05 11:44:50 +0530

    feat: Book Shelf Change,Search Books

---
+++ src/BookGrid.js
@@ -0,0 +1,48 @@
+import React,{ Component } from 'react'
+import PropTypes from 'prop-types';
+
+class BookGrid extends Component {
+
+ render(){
+
+ 	const { books,onShelfChange }=this.props
+
+ 	return (
+ 		<ol className="books-grid">
+
+ 				{books.map((book) => {
+
+						return <li key={book.id}>
+		                        <div className="book">
+		                          <div className="book-top">
+		                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("'+book.imageLinks.thumbnail+'")' }}></div>
+		                            <div className="book-shelf-changer">
+		                              <select value={book.shelf} onChange={(e) => onShelfChange(book,e.target.value)}>
+		                                <option value="none" disabled>Move to...</option>
+		                                <option value="currentlyReading">Currently Reading</option>
+		                                <option value="wantToRead">Want to Read</option>
+		                                <option value="read">Read</option>
+		                                <option value="none">None</option>
+		                              </select>
+		                            </div>
+		                          </div>
+		                          <div className="book-title">{book.title}</div>
+		                          <div className="book-authors">{book.authors !== undefined ? book.authors.join(', ') : ''}</div>
+		                        </div>
+		                      </li>
+
+ 				})}
+
+        </ol>
+    	)
+	}
+
+}
+
+BookGrid.propTypes = {
+
+	books:PropTypes.array.isRequired,
+	onShelfChange:PropTypes.func.isRequired
+}
+
+export default BookGrid
